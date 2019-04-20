a0844aea46b6743c793e6ead79e07c71e170df36
nghi <nguyenhuunghiceo@gmail.com>
2019-04-20 19:56:37 +0700

    config

--- config/__init__.py
+++ config/__init__.py
@@ -1,10 +1,10 @@
 import sys, psycopg2
 
 # run postgres at heroku
-# conn = psycopg2.connect("host='ec2-54-197-253-122.compute-1.amazonaws.com' dbname='df3192mkr3k3ch' user='egtavjsibewktj' password='51fa705edf6ebe7f7e7e3f2223b9768386741a7d91bc731931820e2c7dc8f95d'")
+conn = psycopg2.connect("host='ec2-54-197-253-122.compute-1.amazonaws.com' dbname='df3192mkr3k3ch' user='egtavjsibewktj' password='51fa705edf6ebe7f7e7e3f2223b9768386741a7d91bc731931820e2c7dc8f95d'")
 
 # run postgres at local
-conn = psycopg2.connect("host='localhost' dbname='postgres' user='nghi' password='nghi1996'")
+# conn = psycopg2.connect("host='localhost' dbname='postgres' user='nghi' password='nghi1996'")
 
 cur = conn.cursor()
 
